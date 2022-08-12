30e9f3d1fab295d00bc5a98d30e762fa37695dd2
luiseliasdavid <luis.elias.david@gmail.com>
2022-08-12 00:34:35 -0300

    agregando my port

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 
+ 
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); 
+ const myPort= process.env.PORT || 3001
+  server.listen(myPort, () => {
+    console.log('%s listening at '+ myPort); 
   });
 });
